4058878c2137e6f5367b5e57d7121f76d1d490fb
akashkatakam18 <118911678+akashkatakam18@users.noreply.github.com>
2022-11-25 14:00:22 +0530

    Add files via upload

---
+++ day 4.sql
@@ -0,0 +1,4 @@
+use students;
+create table students(uid varchar (50),First_name varchar(20),Last_name varchar(30),program_c0de varchar(50));
+insert into students values ("1jjGbDfobgZMa4GJ4ui0yCqnTgH3","Akash","Katakam","EJAVANOV122"),("Sq2TBz80GLaU1c01I4PSAJL4J6s1","Ajay","Katakam","EJSNOV122"),("EJSNOV122","sravani","pasupunoor","EJAVANOV122");
+select First_name "Firstname",Last_name"lastname" from students;
